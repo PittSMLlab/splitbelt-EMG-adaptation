130345acd603a9b161a8b0e052e5bd0c93cc1686
Unknown <itu.pablo@gmail.com>
2018-06-26 19:36:51 -0400

    Added contours to fig 3

--- pubfig/F3.m
+++ pubfig/F3.m
@@ -54,7 +54,7 @@ for i=1:length(axB)
         end
 
         %Change contour colors for late adapt:
-        ll=findobj(gca,'type','surface');
+        ll=findobj(gca,'type','contour');
         if ~isempty(ll)
             auxZ=ll.ZData;
             auxZ(571:end,:)=0;
@@ -83,5 +83,19 @@ for i=1:length(axB)
 end
 
 
+%% Add contours 
+s1=findobj(axB(1),'Type','surface'); 
+s2=findobj(axB(2),'Type','surface'); 
+c1=s1.CData; 
+c2=s2.CData; 
+matchedC=sign(c1)==sign(c2) & abs(c1)>.1 & abs(c2)>.1; 
+matchedC(:,end)=0; 
+matchedC(end,:)=0; 
+matchedC=[zeros(1,size(matchedC,2)+1); zeros(size(matchedC,1),1) matchedC; ]; 
+mC=3*interp2([-1:30],[-1:12]',matchedC',[-1:.1:30],[-1:.1:12]','nearest')'; 
+axes(axB(1)) 
+contour3([-.5:.1:12.5]/12+.05/12,[-1:.1:30]'+.55,mC,2.9*[1 1],'k','LineWidth',3,'Clipping','off') 
+axes(axB(2)) 
+contour3([-.5:.1:12.5]/12+.05/12,[-1:.1:30]'+.55,mC,2.9*[1 1],'k','LineWidth',3,'Clipping','off') 
 %%
 saveFig(fB,'./','Fig3',1)
