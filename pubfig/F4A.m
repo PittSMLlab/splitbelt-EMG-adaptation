d523c11a9298e5b81e7f70c17afe7520b45be41c
Unknown <itu.pablo@gmail.com>
2019-01-11 21:11:35 -0500

    Updated fig 4a

--- pubfig/F4A.m
+++ pubfig/F4A.m
@@ -1,14 +1,16 @@
 %From makeN19DPrettyAgain
 saveDir='./';
-name={'allChangesEMG.fig','allChangesEMG_lateAdapBase.fig'};
-desiredPlotDescription={'early','A[5]';'early','P[5]'};
-plotTitles={'Tied-to-split transition','    Split-to-tied transition'};
+name={'allChangesEMG.fig','allChangesEMG.fig','allChangesEMG_lateAdapBase.fig'};
+desiredPlotDescription={'early','A[5]';'early','A[5]';'early','P[5]'};
+plotTitles={'-\Delta EMG_{\uparrow on}','\Delta EMG_{\downarrow on}','    \Delta EMG_{\uparrow off}^{long}'};
 saveName='Fig4A';
 lineFlag=0;
 makeN19DPrettyAgain_execute
 
 %%
 allSS=findobj(gcf,'Type','Surface');
+allSS(3).CData=-allSS(3).CData;
+allSS(2).CData=allSS(2).CData([16:30,1:15],:);
 for i=1:length(allSS)
     ss=allSS(i);
 %ss.CData(16,:)=0;
@@ -19,7 +21,7 @@ idx=strcmp(get(tt,'String'),'FAST/NON-PARETIC');
 set(tt(idx),'String','FAST/DOMINANT');
 end
 %Colormap:
-figuresColorMap
+myFiguresColorMap
 colormap(flipud(map))
 Clim=.5;
 caxis(Clim*[-1 1])
@@ -28,4 +30,7 @@ set(cc,'Ticks',[-.5 -.25 0 .25 .5],'FontSize',16,'FontWeight','bold');
 set(cc,'TickLabels',{'-50%','-25%','No change','+25%','+50%'});
 set(gcf,'Color',ones(1,3))
 %%
-saveFig(gcf,saveDir,saveName,0)
+axB=findobj(gcf,'Type','Axes');
+threePanelArrange
+%%
+%saveFig(gcf,saveDir,saveName,0)
